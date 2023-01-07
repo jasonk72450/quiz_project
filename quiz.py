34b8440df2e5ed110dcde9c1d6eae6a0c58033ab
jasonk72450 <totalhometour@gmail.com>
2023-01-07 15:15:10 -0600

    #2 1.7.23

--- quiz.py
+++ quiz.py
@@ -1,6 +1,10 @@
 # This is a simple quiz program
 # https://realpython.com/python-quiz-application/#demo-your-python-quiz-application
 
+import random
+from string import ascii_lowercase
+
+NUM_QUESTIONS_PER_QUIZ = 5
 QUESTIONS = {
     "When was the first known use of the word 'quiz'": [
         "1781", "1771", "1871", "1881"
@@ -18,13 +22,29 @@ QUESTIONS = {
         "To get information from the user",
     ],
 }
-for question, alternatives in QUESTIONS.items():
-    correct_answer = alternatives[0]
-    for alternative in sorted(alternatives):
-        print(f" - {alternative}")
 
-    answer = input(f"{question}? ")
+num_questions = min(NUM_QUESTIONS_PER_QUIZ, len(QUESTIONS))
+questions = random.sample(list(QUESTIONS.items()), k=num_questions)
+
+num_correct = 0
+for num, (question, alternatives) in enumerate(questions, start=1):
+    print(f"\nQuestion {num}:")
+    print(f"{question}?")
+    correct_answer = alternatives[0]
+    labeled_alternatives = dict(
+        zip(ascii_lowercase, random.sample(alternatives, k=len(alternatives)))
+    )
+    for label, alternative in labeled_alternatives.items():
+        print(f"  {label}) {alternative}")
+
+    while (answer_label := input("\nChoice? ")) not in labeled_alternatives:
+        print(f"Please answer one of {', '.join(labeled_alternatives)}")
+
+    answer = labeled_alternatives[answer_label]
     if answer == correct_answer:
-        print("Correct!")
+        num_correct += 1
+        print("* Correct! *")
     else:
         print(f"The answer is {correct_answer!r}, not {answer!r}")
+
+print(f"\nYou got {num_correct} correct out of {num} questions")
